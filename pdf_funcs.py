d0dbf4ce29d9ad25d35a632fbe4746c2264b0b40
felix.u.shl@gmail.com <felix.u.shl@gmail.com>
2024-04-03 19:51:40 +0300

    Таблица переделана в 5*6 QR на лист

--- pdf_funcs.py
+++ pdf_funcs.py
@@ -10,32 +10,32 @@ def newpage(pdf: FPDF):
     pdf.set_xy(140, 10)
     pdf.set_font("Helvetica", size=16)
     pdf.multi_cell(w=60, text="https://proxway-ble.ru\n8 800 700-19-57\ninfo@proxway-ble.ru")
-    pdf.set_font("Helvetica", size=9)
+    pdf.set_font("Helvetica", size=6.5)
 
 #Отрисовка ячейки с QR кодом и расшифровкой
 def draw_cell(pdf: FPDF, x:int, y:int, qr: list):
     pdf.set_xy(x, y)
-    pdf.image(qr[0], w=45)
-    pdf.multi_cell(w=45, text=f"{qr[1]}\n{qr[2]}\n{qr[3]}")
+    pdf.image(qr[0], w=30)
+    pdf.multi_cell(w=31, text=f"{qr[1]}\n{qr[2]}\n{qr[3]}")
 
 def generate_table(pdf: FPDF, data: list):
     step_y = 30  # стартовый отступ от верхнего края
-    step_x = 10  # стартовый отступ от бокового края
+    step_x = 20  # стартовый отступ от бокового края
     step = 1  # считаем до 4х - кол-во столбцов
     for elem in data:
         draw_cell(pdf, step_x, step_y, elem)
-        if step != 4:
-            step_x += 50
+        if step != 5:
+            step_x += 37
             step += 1
         else:
-            if (data.index(elem) + 1) % 16 == 0 and (data.index(elem) + 1) != len(data):
+            if (data.index(elem) + 1) % 30 == 0 and (data.index(elem) + 1) != len(data):
                 newpage(pdf)
                 step_y = 30  # стартовый отступ от верхнего края
-                step_x = 10  # стартовый отступ от бокового края
+                step_x = 20  # стартовый отступ от бокового края
 
             else:
-                step_x = 10
-                step_y += 62
+                step_x = 20
+                step_y += 42
             step = 1  # начинаем счет до 4х заново
 
 def print_pdf(pdf: FPDF,name: str = 'QR PWID.pdf'):
